9ad12f9c91cef5dac41f094117b634b7cf7ae979
s0l0c0ding <s0l0c0ding@github.com>
2020-11-26 21:03:40 +0100

    :art: style in progress

--- src/components/CountDown.js
+++ src/components/CountDown.js
@@ -1,12 +1,8 @@
 function CountDown(props) {
-    const min = Math.floor(props.current/ 60);
-    const sec = props.current-min*60;
-    function addZeros(num){
-        return num <10 ? '0'+num : num;
-    }
+
     return(
     <div id='time-left'>
-     {addZeros(min) + ':'+ addZeros(sec)}
+     {props.display}
     </div>
     )
 }
--- src/components/Timer.js
+++ src/components/Timer.js
@@ -2,6 +2,8 @@ import { connect } from "react-redux";
 import { startStopAction, countDownAction, resetAction, breakDcrementAction, breakIncrementAction, sessionDecrementAction, sessionIncrementAction } from '../redux/actions';
 import CountDown from "./CountDown";
 import React from "react";
+import { Row, Col, Card, Button, Progress } from 'antd';
+import { CaretDownOutlined, CaretUpOutlined } from '@ant-design/icons'
 
 function Timer(props) {
 
@@ -9,39 +11,63 @@ function Timer(props) {
         const timOut = () => setTimeout(() => props.countDownAction(), 1000);
         if (props.isRunning) {
             timOut();
-            clearTimeout(timOut);
         }
+        clearTimeout(timOut);
     });
-
+    
+    const activeValue = props.isSession ? props.session : props.break;
+    
+    const min = Math.floor(props.current/ 60);
+    const sec = props.current-min*60;
+    function addZeros(num){
+        return num <10 ? '0'+num : num;
+    }
+    const diplay= addZeros(min) + ':'+ addZeros(sec);
+    
     return (
-        <div>
-            <div id='break-label'>
-                Break Length
-            </div>
-            <div id='break-length'>
-                {props.break}
-            </div>
-            <div id='session-label'>
-                Session Length
-            </div>
-            <div id='session-length'>
-                {props.session}
-            </div>
-
+        <Card bodyStyle={{'min-width': '300px'}}>
+            <Row>
+                <Col span={12}>
+                    <Row justify={'center'}>
+                        <div id='break-label'>
+                            Break Length
+                    </div>
+                    </Row>
+                    <Row justify={"space-around"} >
+                        <Button id='break-decrement' onClick={() => props.breakDcrementAction()} shape="circle" icon={<CaretDownOutlined />} />
+                        <div id='break-length'>
+                            {props.break}
+                        </div>
+                        <Button id='break-increment' onClick={() => props.breakIncrementAction()} shape="circle" icon={<CaretUpOutlined />} />
+                    </Row>
+                </Col>
+                <Col span={12}>
+                    <Row justify={'center'}>
+                        <div id='session-label'>
+                            Session Length
+                     </div>
+                    </Row>
+                    <Row justify={"space-around"} >
+                        <Button id='session-increment' onClick={() => props.sessionIncrementAction()} shape="circle" icon={<CaretUpOutlined />} />
+                        <div id='session-length'>
+                            {props.session}
+                        </div>
+                        <Button id='session-decrement' onClick={() => props.sessionDecrementAction()} shape="circle" icon={<CaretDownOutlined />} />
+                    </Row>
+                </Col>
+            </Row>
             <div id='timer-label'>
                 {props.isSession ? 'Session' : 'Break'}
             </div>
-            <div id='time-left'>
-                <CountDown current={props.current} />
-                </div>
+            <Progress type="circle" percent={(1-props.current/60/activeValue)*100} format={() => diplay}/>
+            <div>
+                <CountDown display={diplay} />
+            </div>
             <audio id='beep' src='https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav'></audio>
-            <button id='start_stop' onClick={() => props.startStopAction()}>start/stop</button>
-            <button id='reset' onClick={() => props.resetAction()}>reset</button>
-            <button id='break-decrement' onClick={() => props.breakDcrementAction()}>break-decrement</button>
-            <button id='break-increment' onClick={() => props.breakIncrementAction()}>break-increment</button>
-            <button id='session-increment' onClick={() => props.sessionIncrementAction()}>session-increment</button>
-            <button id='session-decrement' onClick={() => props.sessionDecrementAction()}>session-decrement</button>
-        </div>
+            <Button id='start_stop' onClick={() => props.startStopAction()}>start/stop</Button>
+            <Button id='reset' onClick={() => props.resetAction()}>reset</Button>
+
+        </Card>
     )
 }
 
