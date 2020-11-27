b329a7589486319694a07210ce10caf1f8b8c705
s0l0c0ding <s0l0c0ding@github.com>
2020-11-27 20:28:57 +0100

    :art: style finished

--- src/components/CountDown.js
+++ src/components/CountDown.js
@@ -1,8 +1,19 @@
+import { Progress } from 'antd';
+
 function CountDown(props) {
+    const min = Math.floor(props.current/ 60);
+    const sec = props.current-min*60;
+    function addZeros(num){
+        return num <10 ? '0'+num : num;
+    }
+    const diplay= addZeros(min) + ':'+ addZeros(sec);
+    
+    const activeValue = props.isSession ? props.session : props.break;
+    const perc = (1-props.current/60/activeValue)*100;
 
     return(
-    <div id='time-left'>
-     {props.display}
+    <div  id="time-left" >
+       <Progress type="circle" percent={perc} format={() => diplay}/>
     </div>
     )
 }
--- src/components/Timer.js
+++ src/components/Timer.js
@@ -2,8 +2,8 @@ import { connect } from "react-redux";
 import { startStopAction, countDownAction, resetAction, breakDcrementAction, breakIncrementAction, sessionDecrementAction, sessionIncrementAction } from '../redux/actions';
 import CountDown from "./CountDown";
 import React from "react";
-import { Row, Col, Card, Button, Progress } from 'antd';
-import { CaretDownOutlined, CaretUpOutlined } from '@ant-design/icons'
+import { Row, Col, Card, Button } from 'antd';
+import { CaretDownOutlined, CaretUpOutlined, RedoOutlined,PlaySquareOutlined } from '@ant-design/icons'
 
 function Timer(props) {
 
@@ -11,22 +11,13 @@ function Timer(props) {
         const timOut = () => setTimeout(() => props.countDownAction(), 1000);
         if (props.isRunning) {
             timOut();
+            clearTimeout(timOut);
         }
-        clearTimeout(timOut);
     });
-    
-    const activeValue = props.isSession ? props.session : props.break;
-    
-    const min = Math.floor(props.current/ 60);
-    const sec = props.current-min*60;
-    function addZeros(num){
-        return num <10 ? '0'+num : num;
-    }
-    const diplay= addZeros(min) + ':'+ addZeros(sec);
-    
+
     return (
-        <Card bodyStyle={{'min-width': '300px'}}>
-            <Row>
+        <Card bodyStyle={{ 'min-width': '300px' }}>
+            <Row gutter={[16, 24]}>
                 <Col span={12}>
                     <Row justify={'center'}>
                         <div id='break-label'>
@@ -56,17 +47,19 @@ function Timer(props) {
                     </Row>
                 </Col>
             </Row>
+            <Row justify={"center"} gutter={[16, 24]}>
             <div id='timer-label'>
                 {props.isSession ? 'Session' : 'Break'}
             </div>
-            <Progress type="circle" percent={(1-props.current/60/activeValue)*100} format={() => diplay}/>
-            <div>
-                <CountDown display={diplay} />
-            </div>
+            </Row>
+            <Row justify={"center"} gutter={[16, 10]}>
+            <CountDown current={props.current} isSession={props.isSession} session={props.session} break={props.break} />
+            </Row>
             <audio id='beep' src='https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav'></audio>
-            <Button id='start_stop' onClick={() => props.startStopAction()}>start/stop</Button>
-            <Button id='reset' onClick={() => props.resetAction()}>reset</Button>
-
+            <Row justify={"space-around"} gutter={[16, 2]}>
+            <Button id='start_stop' onClick={() => props.startStopAction()} shape={"circle"} icon={<PlaySquareOutlined />}/>
+            <Button id='reset' onClick={() => props.resetAction()} shape={"circle"} icon={<RedoOutlined />}/>
+            </Row>
         </Card>
     )
 }
